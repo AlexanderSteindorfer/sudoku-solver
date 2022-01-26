bf6a7078ae6938cd11bbb0034f63c0136bca7389
Alexander Steindorfer <98160805+AlexanderSteindorfer@users.noreply.github.com>
2022-01-26 12:52:04 +0100

    Adding Sudoku Solver files
    Adding the Sudoku Solver files to the remote repository.

---
+++ README.md
@@ -0,0 +1,16 @@
+# Sudoku Solver
+A naive backtracking algorithm to solve a Sudoku.
+<br/><br/>
+
+### Short explanation
+This Sudoku Solver uses a naive backtracking algorithm to check a Sudoku board for empty fields and what number could be placed in it according to the Sudoku rules. It will continue placing numbers in the board (thereby mutating the input board) until there are no more empty spaces, or the puzzle is not solvable.
+<br/><br/>
+
+### How to use
+
+Open **sudoku_solver.py** and run the code to see the example given. You may place a differnt Sudoku puzzle there by entering the numbers manually.
+<br/><br/>
+
+### What I learned
+
+Combination of recursion and backtracking in a simple way.
---
+++ sudoku_solver.py
@@ -0,0 +1,78 @@
+from pprint import pprint
+
+
+def find_next_empty(sudoku):
+    # Find an empty spot on the Sudoku board.
+
+    for r in range(9):
+        for c in range(9):
+            if sudoku[r][c] == -1:
+                return r, c
+
+    return None, None
+
+
+def is_valid(sudoku, guess, row, col):
+    # Checks whether our guessed number is already in the row, column, or 3x3 matrix.
+
+    row_vals = sudoku[row]
+    if guess in row_vals:
+        return False
+
+    col_vals = [sudoku[i][col] for i in range(9)]
+    if guess in col_vals:
+        return False
+
+    # Calculates the starting indices of the row and column of the 3x3 matrix we want to look at.
+    row_start = (row // 3) * 3
+    col_start = (col // 3) * 3
+
+    # Iterates over the matrix. +3 because we want to iterate over three rows and columns.
+    for r in range(row_start, row_start +3):
+        for c in range(col_start, col_start +3):
+            if sudoku[r][c] == guess:
+                return False
+
+    return True
+
+
+def solve_sudoku(sudoku):
+    row, col = find_next_empty(sudoku)
+
+    if row is None:
+        return True
+
+    for guess in range(1, 10):
+        if is_valid(sudoku, guess, row, col):
+            # Mutates the Sudoku board by placing our guess at the empty field.
+            sudoku[row][col] = guess
+            # Recursively calls our function with the now mutated Sudoku board as input.
+            # If this returns True, the Sudoku is solved.
+            if solve_sudoku(sudoku):
+                return True
+
+        # Backtracking:
+        # Resets the value at our index to empty in case our guess is not valid, or didn't solve the Sudoku.
+        sudoku[row][col] = -1
+
+    return False
+
+
+if __name__ == '__main__':
+
+    sudoku_example = [
+        [3, 9, -1,   -1, 5, -1,   -1, -1, -1],
+        [-1, -1, -1,   2, -1, -1,   -1, -1, 5],
+        [-1, -1, -1,   7, 1, 9,   -1, 8, -1],
+
+        [-1, 5, -1,   -1, 6, 8,   -1, -1, -1],
+        [2, -1, 6,   -1, -1, 3,   -1, -1, -1],
+        [-1, -1, -1,   -1, -1, -1,   -1, -1, 4],
+
+        [5, -1, -1,   -1, -1, -1,   -1, -1, -1],
+        [6, 7, -1,   1, -1, 5,   -1, 4, -1],
+        [1, -1, 9,   -1, -1, -1,   2, -1, -1]
+    ]
+
+    print(solve_sudoku(sudoku_example))
+    pprint(sudoku_example)
